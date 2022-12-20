4b618355331b61defe77592672c56b10a0b92628
ailkhamov-zaizi <ailkhamov@zaizi.com>
2022-12-20 11:34:50 +0000

    AYR-139 - Removed providers from tf files

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -5,9 +5,4 @@ module "da-vpc" {
   enable_nat_gateway = var.enable_nat_gateway
   project_name = var.project_name
   aws_account_id = var.aws_account_id
-  providers = {
-    aws = aws
-    # aws.us-east-1 = aws.us-east-1
-    aws.eu-west-2 = aws.us-east-1
-  }
 }

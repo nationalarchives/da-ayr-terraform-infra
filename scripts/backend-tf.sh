f745a2be8d88a517ec60f11fdd9bfb03f55dd9c2
Ola Apata <oapata@zaizi.com>
2023-01-06 13:51:44 +0000

    Addition of retrieval of stored configs from parameter store before terraform runs.

--- scripts/backend-tf.sh
+++ scripts/backend-tf.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-#Get values from parameter store and generate backend-config.auto.tfvars
+#Get values from parameter store and generate backend-config.auto.tfvars and terraform.tf vars
+
+apt-get install jq -y
+cd terraform/environment/dev
+
+TF_VARS=$(aws ssm get-parameter --name "terraform-tfvars" --region eu-west-2 | jq .Parameter.Value | sed -r 's/["]+//g' | sed "s/'/\"/g" )
+echo $TF_VARS > terraform.tfvars
+
+TF_AUTO_VARS=$(aws ssm get-parameter --name "backend-config-auto-tfvars" --region eu-west-2 | jq .Parameter.Value | sed -r 's/["]+//g' | sed "s/'/\"/g" )
+echo $TF_AUTO_VARS > backend-config.auto.tfvars

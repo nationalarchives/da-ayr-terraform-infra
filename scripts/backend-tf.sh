34a28ebc76ec41f28c76bb6b088df3d61af11c44
ailkhamov-zaizi <ailkhamov@zaizi.com>
2023-01-08 14:09:35 +0000

    CI: testing

--- scripts/backend-tf.sh
+++ scripts/backend-tf.sh
@@ -5,7 +5,7 @@
 #Get values from parameter store and generate backend-config.auto.tfvars and terraform.tf vars
 
 sudo apt-get install jq -y
-cd terraform/environment/dev
+cd terraform/environments/dev
 
 TF_VARS=$(aws ssm get-parameter --name "terraform-tfvars" --region eu-west-2 | jq .Parameter.Value | sed -r 's/["]+//g' | sed "s/'/\"/g" )
 echo $TF_VARS > terraform.tfvars

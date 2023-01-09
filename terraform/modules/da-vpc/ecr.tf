e7a75a3c9f8fd3b3381fc1eae1d333c2973c3b4c
Ola Apata <oapata@zaizi.com>
2023-01-09 13:10:02 +0000

    Adding ecr to project

---
+++ terraform/modules/da-vpc/ecr.tf
@@ -0,0 +1,8 @@
+ resource "aws_ecr_repository" "ecr" {
+   name                 = "${var.project_name}-django_app-${var.environment}"
+   image_tag_mutability = "MUTABLE"
+
+   image_scanning_configuration {
+     scan_on_push = true
+   }
+ }

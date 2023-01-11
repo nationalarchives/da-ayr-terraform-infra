eedcf8daf057704edc3c4d36728446bc67f84f9b
ailkhamov-zaizi <ailkhamov@zaizi.com>
2023-01-11 20:02:12 +0000

    CI: Testing deployment

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,13 +1,22 @@
-module "da-vpc" {
-  source = "../../modules/da-vpc"
-  vpc_cidr = var.vpc_cidr
-  environment = var.environment
-  enable_nat_gateway = var.enable_nat_gateway
-  project_name = var.project_name
-  aws_account_id = var.aws_account_id
-  providers = {
-    aws = aws
-    # aws.us-east-1 = aws.us-east-1
-    aws.eu-west-2 = aws.us-east-1
+# module "da-vpc" {
+#   source = "../../modules/da-vpc"
+#   vpc_cidr = var.vpc_cidr
+#   environment = var.environment
+#   enable_nat_gateway = var.enable_nat_gateway
+#   project_name = var.project_name
+#   aws_account_id = var.aws_account_id
+#   providers = {
+#     aws = aws
+#     # aws.us-east-1 = aws.us-east-1
+#     aws.eu-west-2 = aws.us-east-1
+#   }
+# }
+resource "aws_vpc" "test" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  provider         = aws.nonprod
+
+  tags = {
+    Name = "ayr-testing-non-prod"
   }
 }
--- terraform/environments/dev/providers.tf
+++ terraform/environments/dev/providers.tf
@@ -15,6 +15,23 @@ provider "aws" {
     }
   }
 
+provider "aws" {
+  alias = "nonprod"
+  ## Default to using London region - all AWS configuration should be
+  ## done in the environment, to use a profile for local testing
+  ## set the `AWS_PROFILE` environment variable appropriately
+  region = "eu-west-2"
+
+  ## All resources that can be tagged should have a base set of metadata
+  ## included as tags - this is an example:
+  default_tags {
+    tags = {
+      Environment = var.environment
+      Owner = "Terraform"
+      StateBucket = var.bucket
+      StatePrefix = var.key
+    }
+  }
   ## OPTIONAL - in some environments we make use of CloudCustodian to tag all 
   ##            resources with the id of its creator.  This section will
   ##            ignore those tag changes so TF doesn't want to update things

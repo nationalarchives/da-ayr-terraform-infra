6c7a6aadbb2bf51309fd1bfb2dbe5893747f24e9
Ola Apata <oapata@zaizi.com>
2023-01-12 16:08:40 +0000

    Realign State file with Github role 2

--- scripts/backend-tf.sh
+++ scripts/backend-tf.sh
@@ -3,15 +3,15 @@
 #Get values from parameter store and generate backend-config.auto.tfvars and terraform.tfvars to be used in build
 #sudo apt-get install jq -y
 pwd
-cd terraform/environments/dev
+# cd terraform/environments/dev
 
 TF_VARS=$(aws ssm get-parameter --name "terraform-tfvars" --region eu-west-2 | jq .Parameter.Value | sed -r 's/["]+//g' | sed "s/'/\"/g" )
 echo $TF_VARS > terraform.tfvars
-cat terraform.tfvars
+# cat terraform.tfvars
 
 TF_AUTO_VARS=$(aws ssm get-parameter --name "backend-config-auto-tfvars" --region eu-west-2 | jq .Parameter.Value | sed -r 's/["]+//g' | sed "s/'/\"/g" )
 echo $TF_AUTO_VARS > backend-config.auto.tfvars
-cat backend-config.auto.tfvars
+# cat backend-config.auto.tfvars
 
 pwd
 ls -alt

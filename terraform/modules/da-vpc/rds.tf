de88ea386fb9bf05b44a2b40be34de8c499e746f
Ola Apata <oapata@zaizi.com>
2023-01-16 11:23:14 +0000

    Updating New Resources

--- terraform/modules/da-vpc/rds.tf
+++ terraform/modules/da-vpc/rds.tf
@@ -6,7 +6,7 @@ resource "aws_db_instance" "mydb1" {
   engine                   = "postgres"
   engine_version           = "13.7"
   identifier               = "mydb1"
-  instance_class           = "db.md5.large"
+  instance_class           = "db.m5d.large"
   multi_az                 = false
   db_name                  = "mydb1"
   parameter_group_name     = "mydbparamgroup1" # if you have tuned it

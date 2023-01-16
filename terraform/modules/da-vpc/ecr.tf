5182432453d2ae1a44cbf9b6dd6c9811db6020c3
Ola Apata <oapata@zaizi.com>
2023-01-16 16:15:55 +0000

    Adding Repositories for keycloak and webapp

--- terraform/modules/da-vpc/ecr.tf
+++ terraform/modules/da-vpc/ecr.tf
@@ -39,3 +39,48 @@
 }
 EOF
 }
+
+
+
+
+resource "aws_ecr_repository" "ecr_keycloak" {
+   name                 = "${var.project_name}-keycloak-${var.environment}"
+   image_tag_mutability = "MUTABLE"
+  
+   image_scanning_configuration {
+     scan_on_push = true
+   }
+ }
+
+ resource "aws_ecr_repository_policy" "ecr_keycloak_policy" {
+  repository = aws_ecr_repository.ecr_keycloak.name
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "new policy",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:PutImage",
+                "ecr:InitiateLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:CompleteLayerUpload",
+                "ecr:DescribeRepositories",
+                "ecr:GetRepositoryPolicy",
+                "ecr:ListImages",
+                "ecr:DeleteRepository",
+                "ecr:BatchDeleteImage",
+                "ecr:SetRepositoryPolicy",
+                "ecr:DeleteRepositoryPolicy"
+            ]
+        }
+    ]
+}
+EOF
+}

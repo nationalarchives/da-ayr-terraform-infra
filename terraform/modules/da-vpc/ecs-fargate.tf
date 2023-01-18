28081f7448a8c64880e78d77ba4a0a61cea4ff94
Ola Apata <oapata@zaizi.com>
2023-01-18 12:27:42 +0000

    Adding new parameters to infra

--- terraform/modules/da-vpc/ecs-fargate.tf
+++ terraform/modules/da-vpc/ecs-fargate.tf
@@ -245,10 +245,20 @@ resource "aws_ecs_task_definition" "definition" {
     },
     "environment": [
       {
-      "name": "WEBAPP_DB_NAME", "value": "django",
-      "name": "WEBAPP_DB_HOST", "value": "test",
-      "name": "WEBAPP_DEBUG", "value": "false",
-      "name": "WEBAPP_DB_PASSWORD", "value": "dj4ng0",
+       "name": "WEBAPP_DB_NAME", 
+       "value": "django"
+      },
+      {
+       "name": "WEBAPP_DB_HOST", 
+       "value": "test"
+      },
+      {
+       "name": "WEBAPP_DEBUG", 
+       "value": "false"
+      },
+      {
+       "name": "WEBAPP_DB_PASSWORD", 
+       "value": "dj4ng0",
       }
     ],
     "portMappings": [

10652bc58266fe2fe7551e55be2403bc01e79d96
Ola Apata <oapata@zaizi.com>
2023-01-18 14:47:24 +0000

    Adding updating  parameters

--- terraform/modules/da-vpc/elb-keycloak.tf
+++ terraform/modules/da-vpc/elb-keycloak.tf
@@ -17,7 +17,7 @@ resource "aws_lb" "loadbalancer-keycloak" {
 
 resource "aws_lb_target_group" "lbtargets-keycloak" {
   name = "tf-lb-target-keycloak-${var.environment}"
-  port = var.app_port
+  port = var.app_port_keycloak
   protocol = "HTTP"
   vpc_id = module.vpc.vpc_id
   target_type = "ip"
@@ -32,7 +32,7 @@ resource "aws_lb_target_group" "lbtargets-keycloak" {
 }
 
 resource "aws_lb_listener" "httpslistener-keycloak" {
-  load_balancer_arn = aws_lb.loadbalancer.arn
+  load_balancer_arn = aws_lb.loadbalancer-keycloak.arn
   port = "80"
   protocol = "HTTP"
   #certificate_arn = aws_acm_certificate_validation.cert-validation.certificate_arn

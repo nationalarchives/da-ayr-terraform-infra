cbd13e2c0be13f8ee45f5deb7e5c1f02e460c578
Ola Apata <oapata@zaizi.com>
2023-01-20 13:54:04 +0000

    Setting up cert resources

---
+++ terraform/environments/providers.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region                  = "eu-west-2"
+  # profile                 = "nhsbsa"
+  # assume_role {
+  #   role_arn = "arn:aws:iam::626513967161:role/Zaizi-Cloud-Engineer"
+  # }
+  assume_role {
+    role_arn =  var.assume_role.nonprod
+  }
+}

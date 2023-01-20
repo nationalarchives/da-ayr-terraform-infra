c14eb06603e245e195f234a83e055693878dd22c
Ola Apata <oapata@zaizi.com>
2023-01-20 12:34:24 +0000

    Setting up cert resources

--- terraform/environments/dev/providers.tf
+++ terraform/environments/dev/providers.tf
@@ -41,6 +41,19 @@ terraform {
     }
   }
 
+provider "aws" {
+  alias = "us-east-1"
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      Environment = var.environment
+      Owner = "Terraform"
+      StateBucket = var.bucket
+      StatePrefix = var.key
+    }
+  }
+}
+
   ## Backend should be configured with terraform init, eg:
   ##   `terraform init -backend-config=backend-config.auto.tfvars -reconfigure`
   ## This allows for keeping terraform code DRY between environments

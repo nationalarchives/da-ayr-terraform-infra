9ffe5dc4c4d3f5e0bbd887bdb76bebb00b2724b5
Ola Apata <oapata@zaizi.com>
2023-01-21 20:05:05 +0000

    Setting up cert resources

--- terraform/modules/da-vpc/elb-keycloak.tf
+++ terraform/modules/da-vpc/elb-keycloak.tf
@@ -31,6 +31,22 @@ resource "aws_lb_target_group" "lbtargets-keycloak" {
   }
 }
 
+resource "aws_lb_target_group" "lbtargets-keycloak-1" {
+  name = "tf-lb-target-keycloak-1-${var.environment}"
+  port = var.app_port_keycloak
+  protocol = "HTTP"
+  vpc_id = module.vpc.vpc_id
+  target_type = "ip"
+
+  health_check {
+    enabled = true
+    matcher = "200-399"
+    port = "traffic-port"
+    protocol = "HTTP"
+    timeout = 10
+  }
+}
+
 resource "aws_lb_listener" "httpslistener-keycloak" {
   load_balancer_arn = aws_lb.loadbalancer-keycloak.arn
   port = "80"
@@ -40,6 +56,18 @@ resource "aws_lb_listener" "httpslistener-keycloak" {
   default_action {
     type = "forward"
     target_group_arn = aws_lb_target_group.lbtargets-keycloak.arn
+  }
+}
+
+resource "aws_lb_listener" "httpslistener-keycloak-1" {
+  load_balancer_arn = aws_lb.loadbalancer.arn
+  port = "443"
+  protocol = "HTTPS"
+  certificate_arn = aws_acm_certificate_validation.cert-validation.certificate_arn
+  
+  default_action {
+  type = "forward"
+    target_group_arn = aws_lb_target_group.lbtargets-keycloak-1.arn
   }
 }
 

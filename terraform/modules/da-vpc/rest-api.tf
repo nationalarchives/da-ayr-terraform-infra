b1a18689e3a71276047624fa51a29cad1cecf2e1
Ola Apata <oapata@zaizi.com>
2023-02-17 14:00:06 +0000

    adding opensearch config and test lambda resource

--- terraform/modules/da-vpc/rest-api.tf
+++ terraform/modules/da-vpc/rest-api.tf
@@ -38,7 +38,7 @@ resource "aws_lambda_function" "lambda_rest_api" {
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = filebase64sha256("lambda_rest_api.zip")
+  source_code_hash = filebase64sha256("../../../lambda/lambda_rest_api.zip")
 
   runtime = "puthon3.9"
 

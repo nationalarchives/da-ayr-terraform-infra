d269ff2e716615429161c0a1f9e465a70a77562f
ailkhamov-zaizi <ailkhamov@zaizi.com>
2023-02-23 13:10:31 +0000

    CI: Testing new deploy

--- terraform/modules/da-vpc/api-gateway.tf
+++ terraform/modules/da-vpc/api-gateway.tf
@@ -36,6 +36,19 @@ EOF
   }
 }
 
+resource "aws_api_gateway_resource" "da-ayr" {
+  rest_api_id = aws_api_gateway_rest_api.da-ayr-test.id
+  parent_id   = aws_api_gateway_rest_api.da-ayr-test.root_resource_id
+  path_part   = "mydemoresource"
+}
+
+resource "aws_api_gateway_method" "da-ayr" {
+  rest_api_id   = aws_api_gateway_rest_api.da-ayr-test.id
+  resource_id   = aws_api_gateway_resource.da-ayr.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
 
 resource "aws_api_gateway_deployment" "test" {
   rest_api_id = "${aws_api_gateway_rest_api.da-ayr-test.id}"

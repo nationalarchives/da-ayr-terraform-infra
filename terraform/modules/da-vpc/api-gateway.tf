e47131622795e3906b660763006f4976a5b1354d
ailkhamov-zaizi <ailkhamov@zaizi.com>
2023-02-23 17:47:06 +0000

    CI: Testing

--- terraform/modules/da-vpc/api-gateway.tf
+++ terraform/modules/da-vpc/api-gateway.tf
@@ -55,6 +55,14 @@ resource "aws_api_gateway_stage" "stage-test" {
   stage_name    = "test"
 }
 
+resource "aws_api_gateway_integration" "test_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.da-ayr-test.id
+  resource_id             = aws_api_gateway_resource.da-ayr.id
+  http_method             = aws_api_gateway_method.da-ayr.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda_rest_api.invoke_arn
+}
 
 resource "aws_api_gateway_deployment" "test" {
   rest_api_id = aws_api_gateway_rest_api.da-ayr-test.id
@@ -64,15 +72,6 @@ resource "aws_api_gateway_deployment" "test" {
         aws_api_gateway_method.da-ayr,
         aws_api_gateway_integration.test_integration
       ]
-}
-
-resource "aws_api_gateway_integration" "test_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.da-ayr-test.id
-  resource_id             = aws_api_gateway_resource.da-ayr.id
-  http_method             = aws_api_gateway_method.da-ayr.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.lambda_rest_api.invoke_arn
 }
 
 output "api-url" {

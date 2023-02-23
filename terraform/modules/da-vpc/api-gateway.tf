766923e79502dc3a0d726f396be8e2d5e7a5994b
ailkhamov-zaizi <ailkhamov@zaizi.com>
2023-02-23 15:52:37 +0000

    CI: Testing

--- terraform/modules/da-vpc/api-gateway.tf
+++ terraform/modules/da-vpc/api-gateway.tf
@@ -60,6 +60,7 @@ resource "aws_api_gateway_deployment" "test" {
 
 resource "aws_api_gateway_integration" "test_integration" {
   rest_api_id             = aws_api_gateway_rest_api.da-ayr-test.id
+  resource_id             = aws_api_gateway_resource.da-ayr.id
   http_method             = aws_api_gateway_method.da-ayr.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"

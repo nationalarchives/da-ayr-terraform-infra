6e27a85a6ae73620f1b21a81cad319f039cc396e
ailkhamov-zaizi <ailkhamov@zaizi.com>
2023-02-23 17:27:01 +0000

    CI: Testing

--- terraform/modules/da-vpc/api-gateway.tf
+++ terraform/modules/da-vpc/api-gateway.tf
@@ -58,7 +58,12 @@ resource "aws_api_gateway_stage" "stage-test" {
 
 resource "aws_api_gateway_deployment" "test" {
   rest_api_id = aws_api_gateway_rest_api.da-ayr-test.id
-  stage_name = "test"
+  stage_name = aws_api_gateway_stage.stage-test.stage_name
+
+  depends_on = [
+        aws_api_gateway_method.da-ayr,
+        aws_api_gateway_integration.test_integration
+      ]
 }
 
 resource "aws_api_gateway_integration" "test_integration" {

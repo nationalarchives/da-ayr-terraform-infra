1cc09a8e8417da22737af55eac81a91c6fb9fcda
ailkhamov-zaizi <ailkhamov@zaizi.com>
2023-02-24 11:18:29 +0000

    CI: Testing new Lamda

--- terraform/modules/da-vpc/api-gateway.tf
+++ terraform/modules/da-vpc/api-gateway.tf
@@ -61,7 +61,7 @@ resource "aws_api_gateway_integration" "test_integration" {
   http_method             = aws_api_gateway_method.da-ayr.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "aws_lambda_function.lambda_rest_api.invoke_arn"
+  uri                     = "arn:aws:lambda:eu-west-2:281072317055:function:dgandy-tmp-test-lambda-1"
 }
 
 resource "aws_api_gateway_deployment" "test" {

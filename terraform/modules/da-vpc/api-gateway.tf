1ccaaea84c519c2719f9077b931946de489c65a2
ailkhamov-zaizi <ailkhamov@zaizi.com>
2023-02-27 15:08:56 +0000

    CI: Added Authorizer to method

--- terraform/modules/da-vpc/api-gateway.tf
+++ terraform/modules/da-vpc/api-gateway.tf
@@ -45,8 +45,9 @@ resource "aws_api_gateway_resource" "da-ayr" {
 resource "aws_api_gateway_method" "da-ayr" {
   rest_api_id   = aws_api_gateway_rest_api.da-ayr-test.id
   resource_id   = aws_api_gateway_resource.da-ayr.id
-  http_method   = "GET"
-  authorization = "NONE"
+  http_method   = "POST"
+  authorization = "CUSTOM"
+  authorizer_id = aws_api_gateway_authorizer.da-ayr-authorizer.id
 }
 
 # resource "aws_api_gateway_stage" "stage-test" {
@@ -59,8 +60,8 @@ resource "aws_api_gateway_integration" "test_integration" {
   rest_api_id             = "${aws_api_gateway_rest_api.da-ayr-test.id}"
   resource_id             = "${aws_api_gateway_resource.da-ayr.id}"
   http_method             = "${aws_api_gateway_method.da-ayr.http_method}"
-  integration_http_method = "GET"
-  type                    = "AWS"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
   uri                     = "${aws_lambda_function.lambda_rest_api.invoke_arn}"
 }
 

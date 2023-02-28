fe8be03ab0f00ddcad24fc21d73d71472da1ac3e
Ola Apata <oapata@zaizi.com>
2023-02-28 18:02:34 +0000

    Lamda and API gateway configuration update

--- terraform/modules/da-vpc/lambda-auth.tf
+++ terraform/modules/da-vpc/lambda-auth.tf
@@ -56,7 +56,7 @@ resource "aws_lambda_permission" "apigw_lambda_auth_permission" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda_auth.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:eu-west-2:${var.aws_account_id}:*"
+  source_arn    = "arn:aws:execute-api:eu-west-2:${var.aws_account_id}:${aws_api_gateway_rest_api.da-ayr.id}/*/${aws_api_gateway_method.da-ayr.http_method}${aws_api_gateway_resource.da-ayr.path}"
   # source_arn = "arn:aws:execute-api:${var.region}:${var.aws_account_id}:*"
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
   # source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"

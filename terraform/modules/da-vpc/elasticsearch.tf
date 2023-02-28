7d984b80a2044b4e54d1ec698d90cbce900066f5
Ola Apata <oapata@zaizi.com>
2023-02-28 16:05:39 +0000

    Opensearch Configuration update

--- terraform/modules/da-vpc/elasticsearch.tf
+++ terraform/modules/da-vpc/elasticsearch.tf
@@ -75,7 +75,7 @@ resource "aws_opensearch_domain" "es" {
     }
 
     advanced_security_options {
-        enabled                        = false
+        enabled                        = true
         anonymous_auth_enabled         = true
         internal_user_database_enabled = true
         master_user_options {
@@ -100,6 +100,15 @@ resource "aws_opensearch_domain" "es" {
     ebs_options {
        ebs_enabled = true
        volume_size = 10
+    }
+
+    encrypt_at_rest {
+       enabled = true
+    }
+
+    domain_endpoint_options {
+       enforce_https       = true
+       tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
     }
 
     vpc_options {

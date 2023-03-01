fb3cfa62bba9cb6d67bba7f1fbb61afafadc78ef
ailkhamov-zaizi <ailkhamov@zaizi.com>
2023-03-01 15:38:49 +0000

    CI: Testing new path

--- terraform/modules/da-vpc/api-gateway.tf
+++ terraform/modules/da-vpc/api-gateway.tf
@@ -35,12 +35,13 @@ EOF
 resource "aws_api_gateway_resource" "da-ayr" {
   rest_api_id = aws_api_gateway_rest_api.da-ayr.id
   parent_id   = aws_api_gateway_rest_api.da-ayr.root_resource_id
-  path_part   = "/"
+  # path_part   = "/"
 }
 
 resource "aws_api_gateway_method" "da-ayr" {
   rest_api_id   = aws_api_gateway_rest_api.da-ayr.id
-  resource_id   = aws_api_gateway_resource.da-ayr.id
+  # resource_id   = aws_api_gateway_resource.da-ayr.id
+  resource_id   = aws_api_gateway_resource.da-ayr.root_resource_id
   http_method   = "POST"
   authorization = "CUSTOM"
   authorizer_id = aws_api_gateway_authorizer.da-ayr-authorizer.id

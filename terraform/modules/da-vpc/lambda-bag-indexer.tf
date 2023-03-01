5b55e88c57031ab24e9ded6ce328b6fed0a64610
Ola Apata <oapata@zaizi.com>
2023-03-01 15:03:09 +0000

    Fixing lambda duplicates

--- terraform/modules/da-vpc/lambda-bag-indexer.tf
+++ terraform/modules/da-vpc/lambda-bag-indexer.tf
@@ -26,13 +26,13 @@ EOF
 }
 
 # resource "aws_lambda_function" "test_lambda" {
-resource "aws_lambda_function" "lambda_auth" {
+resource "aws_lambda_function" "lambda_indexer" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
   filename      = "../../../lambda/lambda_bag_indexer.zip"
   # function_name = "lambda_handler"
   function_name = "${var.project_name}-indexer-${var.environment}"
-  role          = aws_iam_role.iam_for_lambda_auth.arn
+  role          = aws_iam_role.iam_for_lambda_indexer.arn
   handler       = "lambda_handler"
 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later

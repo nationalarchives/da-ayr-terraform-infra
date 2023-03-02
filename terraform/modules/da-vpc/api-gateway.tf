918b11d1414a608f52c4cc18845e4df929d0467d
ailkhamov-zaizi <ailkhamov@zaizi.com>
2023-03-02 13:40:09 +0000

    CI: Removed cache and changed header to foo

--- terraform/modules/da-vpc/api-gateway.tf
+++ terraform/modules/da-vpc/api-gateway.tf
@@ -85,9 +85,9 @@ resource "aws_api_gateway_authorizer" "da-ayr-authorizer" {
   name                   = "da-ayr-authorizer-dev"
   rest_api_id            = aws_api_gateway_rest_api.da-ayr.id
   authorizer_uri         = aws_lambda_function.lambda_auth.invoke_arn
-  identity_source        = "method.request.header.Authorization"
+  identity_source        = "method.request.header.foo"
   type                   = "TOKEN"
-  authorizer_result_ttl_in_seconds = 300
+  authorizer_result_ttl_in_seconds = 0
   identity_validation_expression = ""
 }
 

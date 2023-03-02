1c38c101c7256917679047221ffa9881c3307c07
ailkhamov-zaizi <ailkhamov@zaizi.com>
2023-03-02 14:12:10 +0000

    CI: set path_part   = {proxy+}

--- terraform/modules/da-vpc/api-gateway.tf
+++ terraform/modules/da-vpc/api-gateway.tf
@@ -35,7 +35,7 @@ EOF
 resource "aws_api_gateway_resource" "da-ayr" {
   rest_api_id = aws_api_gateway_rest_api.da-ayr.id
   parent_id   = aws_api_gateway_rest_api.da-ayr.root_resource_id
-  path_part   = "da-ayr-dev"
+  path_part   = "{proxy+}"
 }
 
 resource "aws_api_gateway_method" "da-ayr" {

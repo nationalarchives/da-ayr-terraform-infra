efeb0d314f5dda1fe55dead2cf672c700bb84ea6
Ola Apata <oapata@zaizi.com>
2023-03-03 16:17:18 +0000

    Refactoring lambda iac - creating central policy and roles

--- terraform/modules/da-vpc/lambda_role_policy.tf
+++ terraform/modules/da-vpc/lambda_role_policy.tf
@@ -57,19 +57,19 @@ resource "aws_iam_policy" "iam_lambda_policy" {
     {
       "Effect": "Allow",
       "Action": [
-            "ec2:CreateNetworkInterface",
-            "ec2:DescribeNetworkInterfaces",
-            "ec2:DeleteNetworkInterface",
-            "ec2:AssignPrivateIpAddresses",
-            "ec2:UnassignPrivateIpAddresses"
-        ],
-        "Resource": "*"
+         "ec2:CreateNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DeleteNetworkInterface",
+          "ec2:AssignPrivateIpAddresses",
+          "ec2:UnassignPrivateIpAddresses"
+      ],
+      "Resource": "*"
     },
     {
-      "Action": "ssm:GetParameter",
-            "Effect": "Allow",
-            "Resource": "arn:aws:ssm:eu-west-2:281072317055:parameter/dev/*",
-            "Service": "ssm",
+          "Action": "ssm:GetParameter",
+          "Effect": "Allow",
+          "Resource": "arn:aws:ssm:eu-west-2:281072317055:parameter/dev/*",
+          "Service": "ssm"
     }
   ]
 }

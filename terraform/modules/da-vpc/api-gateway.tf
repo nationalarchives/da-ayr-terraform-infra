ae684c26a402f0d6f4148a4ef99286fbbc05a089
ailkhamov-zaizi <ailkhamov@zaizi.com>
2023-03-07 19:21:47 +0000

    CI: Changing parametor header to Ayr-Access-Token

--- terraform/modules/da-vpc/api-gateway.tf
+++ terraform/modules/da-vpc/api-gateway.tf
@@ -85,7 +85,7 @@ resource "aws_api_gateway_authorizer" "da-ayr-authorizer" {
   name                   = "da-ayr-authorizer-dev"
   rest_api_id            = aws_api_gateway_rest_api.da-ayr.id
   authorizer_uri         = aws_lambda_function.lambda_auth.invoke_arn
-  identity_source        = "method.request.header.foo"
+  identity_source        = "method.request.header.Ayr-Access-Token"
   type                   = "TOKEN"
   authorizer_result_ttl_in_seconds = 0
   identity_validation_expression = ""

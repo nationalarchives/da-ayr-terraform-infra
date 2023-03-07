9ea9fd5b30988639fdfd40ee8fce8e0202739a9e
Ola Apata <oapata@zaizi.com>
2023-03-07 18:34:17 +0000

    Forcing change to API Gateway path

--- terraform/modules/da-vpc/lambda-rest-api.tf
+++ terraform/modules/da-vpc/lambda-rest-api.tf
@@ -78,7 +78,7 @@ resource "aws_lambda_permission" "apigw_lambda_auth_permission" {
   principal     = "apigateway.amazonaws.com"
   #source_arn    = "arn:aws:execute-api:eu-west-2:${var.aws_account_id}:${aws_api_gateway_rest_api.da-ayr.id}/*/${aws_api_gateway_method.da-ayr.http_method}${aws_api_gateway_resource.da-ayr.path}"
   # source_arn    = "arn:aws:execute-api:eu-west-2:${var.aws_account_id}:${aws_api_gateway_rest_api.da-ayr.id}/*/${aws_api_gateway_method.da-ayr.http_method}"
-  source_arn    = "arn:aws:execute-api:eu-west-2:${var.aws_account_id}:${aws_api_gateway_rest_api.da-ayr.id}/*/${aws_api_gateway_method.da-ayr.http_method}"
+  source_arn    = "arn:aws:execute-api:eu-west-2:${var.aws_account_id}:${aws_api_gateway_rest_api.da-ayr.id}/*/POST/*"
 
   # source_arn    = "arn:aws:execute-api:eu-west-2:${var.aws_account_id}:${aws_api_gateway_rest_api.da-ayr.id}/*/POST/*"
   # source_arn = "arn:aws:execute-api:${var.region}:${var.aws_account_id}:*"

31329eeeeab354d15c64e96a508ef8cf4b1883d7
Ola Apata <oapata@zaizi.com>
2023-03-07 12:51:52 +0000

    Updating api lambda

--- terraform/modules/da-vpc/lambda-rest-api.tf
+++ terraform/modules/da-vpc/lambda-rest-api.tf
@@ -69,3 +69,15 @@ resource "aws_cloudwatch_log_group" "function_log_group7" {
     prevent_destroy = false
   }
 }
+
+resource "aws_lambda_permission" "apigw_lambda_auth_permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_rest_api.function_name
+  principal     = "apigateway.amazonaws.com"
+  #source_arn    = "arn:aws:execute-api:eu-west-2:${var.aws_account_id}:${aws_api_gateway_rest_api.da-ayr.id}/*/${aws_api_gateway_method.da-ayr.http_method}${aws_api_gateway_resource.da-ayr.path}"
+  source_arn    = "arn:aws:execute-api:eu-west-2:${var.aws_account_id}:${aws_api_gateway_rest_api.da-ayr.id}/*/${aws_api_gateway_method.da-ayr.http_method}"
+  # source_arn = "arn:aws:execute-api:${var.region}:${var.aws_account_id}:*"
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  # source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+}

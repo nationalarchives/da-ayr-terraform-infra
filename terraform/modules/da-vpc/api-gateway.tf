9563db1d99db2d379f0725502e89e75a861ab651
ailkhamov-zaizi <ailkhamov@zaizi.com>
2023-03-08 12:23:38 +0000

    CI: Changed Path path_part to bag-serach and stage name to alpha

--- terraform/modules/da-vpc/api-gateway.tf
+++ terraform/modules/da-vpc/api-gateway.tf
@@ -35,7 +35,7 @@ EOF
 resource "aws_api_gateway_resource" "da-ayr" {
   rest_api_id = aws_api_gateway_rest_api.da-ayr.id
   parent_id   = aws_api_gateway_rest_api.da-ayr.root_resource_id
-  path_part   = "da-ayr-dev"
+  path_part   = "bag-search"
 }
 
 resource "aws_api_gateway_method" "da-ayr" {
@@ -65,7 +65,7 @@ resource "aws_api_gateway_integration" "test_integration" {
 
 resource "aws_api_gateway_deployment" "test" {
   rest_api_id = aws_api_gateway_rest_api.da-ayr.id
-  stage_name = "test"
+  stage_name = "alpha"
 
   lifecycle {
     create_before_destroy = true

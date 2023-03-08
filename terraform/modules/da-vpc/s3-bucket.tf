6b1be3dea03a6a5b9edcbf61e77ebfa6055d243f
ailkhamov-zaizi <ailkhamov@zaizi.com>
2023-03-08 14:41:29 +0000

    CI: Updated s3 buckets

--- terraform/modules/da-vpc/s3-bucket.tf
+++ terraform/modules/da-vpc/s3-bucket.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "da-ayr-data" {
+resource "aws_s3_bucket" "da_ayr_data" {
   bucket = "da-ayr-data"
   acl    = "private"
 
@@ -7,8 +7,16 @@ resource "aws_s3_bucket" "da-ayr-data" {
     Environment = var.environment
   }
 }
+resource "aws_s3_bucket_public_access_block" "da-ayr-data" {
+  bucket = aws_s3_bucket.da_ayr_data.id
 
-resource "aws_s3_bucket" "tre-bag-files" {
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket" "tre_bag_files" {
   bucket = "tre-bag-files"
   acl    = "private"
 
@@ -16,4 +24,13 @@ resource "aws_s3_bucket" "tre-bag-files" {
     Name        = "tre-bag-files"
     Environment = var.environment
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "tre_bag_files" {
+  bucket = aws_s3_bucket.tre_bag_files.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
